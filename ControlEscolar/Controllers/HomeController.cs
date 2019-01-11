87ea2fc6d6c89b8e695cb4d258e41893f115461a
Omar Munguia <ingscomarmr@gmail.com>
2019-01-10 18:15:20 -0600

    Esto es in test

---
+++ ControlEscolar/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlEscolar.Controllers
+{
+    public class HomeController : Controller
+    {
+        //http://localhost:5534/home/index
+        //http://localhost:5534/home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AcercaDe()
+        {
+            return View();
+        }
+    }
+}
